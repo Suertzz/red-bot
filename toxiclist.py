2f33565b52c360f1dd9187255d3946cb0e0008bc
Stanislas Savidan <stsavida@student.42.fr>
2018-06-30 23:12:40 +0000

    add python file

---
+++ toxiclist.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class ToxicList:
+
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def toxiclist(self):
+        await self.bot.say("Salut bb")
+
+def setup (bot)
+    bot.add_cog(ToxicList(bot))
