a841ed34bb6dd1e90b0f4c7c22e7763283139652
Stanislas Savidan <stsavida@student.42.fr>
2018-07-04 00:19:59 +0000

    test

--- ToxicList/ToxicList.py
+++ ToxicList/ToxicList.py
@@ -5,10 +5,27 @@ class ToxicList:
 
     def __init__(self, bot):
         self.bot = bot
+    
     @commands.command()
     async def toxiclist(self):
         await self.bot.say("Salut bb")
 
+    def is_command(self, msg):
+        if callable(self.bot.command_prefix):
+            prefixes = self.bot.command_prefix(self.bot, msg)
+        else:
+            prefixes = self.bot.command_prefix
+        for p in prefixes:
+            if msg.content.startswith(p):
+                return True
+        return False
+
+
+    async def msg_listener(self, message):
+        if message.author == self.bot.user:
+            return
+        if self.is_command(message):
+            return
 
 def setup(bot):
     bot.add_cog(ToxicList(bot))
